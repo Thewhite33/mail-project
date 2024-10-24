e1e3973b5ecefdb56d186cf9df108b4fb66aff6b
Thewhite33 <rohitrc2005@gmail.com>
2024-10-24 11:57:26 +0530

    end

--- server.js
+++ server.js
@@ -37,6 +37,6 @@ io.on("connection", (socket) => {
 });
 
 // Start the server
-server.listen(3000, () => {
-    console.log("Server running on http://localhost:3000");
+server.listen(5000, () => {
+    console.log("Server running on http://localhost:5000");
 });
